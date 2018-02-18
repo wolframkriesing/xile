59572c234f13b752ea619e04d11d8c3d9093cf12
wolframkriesing <wolfram+github@kriesing.de>
2018-02-18 21:46:45 +0100

    Remove all nice to haves ... wanna get down to the bare bones.

--- src/SlidingPages.js
+++ src/SlidingPages.js
@@ -1,41 +1,14 @@
-import AriaListMixin from './AriaListMixin.js';
 import DirectionSelectionMixin from './DirectionSelectionMixin.js';
 import FocusVisibleMixin from './FocusVisibleMixin.js';
-import KeyboardDirectionMixin from './KeyboardDirectionMixin.js';
-import KeyboardMixin from './KeyboardMixin.js';
 import SlidingViewport from './SlidingViewport.js';
-import SwipeDirectionMixin from './SwipeDirectionMixin.js';
-import TouchSwipeMixin from './TouchSwipeMixin.js';
-import TrackpadSwipeMixin from './TrackpadSwipeMixin.js';
 
 
 const Base =
-  AriaListMixin(
   DirectionSelectionMixin(
   FocusVisibleMixin(
-  KeyboardDirectionMixin(
-  KeyboardMixin(
-  SwipeDirectionMixin(
-  TouchSwipeMixin(
-  TrackpadSwipeMixin(
     SlidingViewport
-  ))))))));
+  ));
 
-
-/**
- * Allows a user to navigate a horizontal set of items with touch, mouse,
- * keyboard, or trackpad. Shows a sliding effect when moving between items.
- * 
- * @inherits SlidingViewport
- * @mixes AriaListMixin
- * @mixes DirectionSelectionMixin
- * @mixes FocusVisibleMixin
- * @mixes KeyboardDirectionMixin
- * @mixes KeyboardMixin
- * @mixes SwipeDirectionMixin
- * @mixes TouchSwipeMixin
- * @mixes TrackpadSwipeMixin
- */
 class SlidingPages extends Base {}
 
 
